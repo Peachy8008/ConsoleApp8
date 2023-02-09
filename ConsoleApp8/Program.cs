f33bf55406eca692c74a4045e4e5d7c0536c3e2b
Adrian <Adrian@DESKTOP-DL3ULRK>
2023-02-09 17:44:29 +0800

    Add project files.

---
+++ ConsoleApp8/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ConsoleApp8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+               //sample program
+            int a = 5;
+            while (a < 300)
+            {
+                Console.WriteLine(a);
+                a = a + 10;
+            }
+        }
+    }
+}
